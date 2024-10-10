9dd3e4790299b736a8bc198521253f2bb03f866a
Guillaume C <81625005+Bledichef@users.noreply.github.com>
2024-10-10 14:30:00 +0200

    Clean & add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../assets/Logo.webp"; 
 
 function Header() {
   return (
-    <header className="bg-gray-800 text-white p-4">
+    <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
+      {/* Navigation */}
       <nav>
         <ul className="flex space-x-4">
           <li>
@@ -17,6 +19,11 @@ function Header() {
           </li>
         </ul>
       </nav>
+
+      {/* Logo avec bordures arrondies */}
+      <div>
+        <img src={Logo} alt="Logo" className="h-20 w-auto rounded-lg" /> {/* Utilise "rounded-lg" pour des bords légèrement arrondis */}
+      </div>
     </header>
   );
 }
